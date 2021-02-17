ac5b0f0b16a65e5ed1e208bcb680db0c421ae989
WisnuCakraa <wisnucakraa@gmail.com>
2021-02-17 19:10:14 +0700

    [ADD] Asset Img

---
+++ FrontEnd Responsive/src/Assets/index.js
@@ -0,0 +1,15 @@
+import Fortuner from "./Fortuner.png";
+import Heart from "./heart_icon.svg";
+import Circle from "./circle.svg";
+import Balance from "./balance.svg";
+import Calculator from "./calculator.svg";
+import Steering from "./steering-wheel.svg";
+
+export const IMAGE = {
+  FORTUNER: Fortuner,
+  HEART: Heart,
+  CIRCLE: Circle,
+  BALANCE: Balance,
+  CALCULATOR: Calculator,
+  STEERING: Steering,
+};
